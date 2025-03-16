95a98c69385db0822ea7fbd1fc979c9bc08a9f0f
sungchan12 <chanyouunng1@gmail.com>
2025-03-16 17:40:35 +0900

    Add files via upload

---
+++ src/test/java/com/example/univTime/univTime/UnivTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.univTime.univTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
