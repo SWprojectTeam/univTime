fcc89929263f60b3d9a598d67ddd5cd31312a2c2
masqueried <codic2459@gmail.com>
2025-03-17 21:27:57 +0900

    Update PostRepository.java

--- src/main/java/com/example/univTime/univTime/repository/PostRepository.java
+++ src/main/java/com/example/univTime/univTime/repository/PostRepository.java
@@ -2,6 +2,10 @@ package com.example.univTime.univTime.repository;
 
 import com.example.univTime.univTime.domain.post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<post, Long> {
+    // 제목에 특정 단어를 포함하는 게시글 찾기
+    List<post> findByTitleContaining(String keyword);
 }
